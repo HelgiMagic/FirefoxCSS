9f4d9c0861961b3bbac7a2bfd049753b6b59ab52
HelgiMagic <113669521+HelgiMagic@users.noreply.github.com>
2024-01-18 20:12:03 +0300

    Update BetterFox_overrides.js

--- BetterFox_overrides.js
+++ BetterFox_overrides.js
@@ -2,3 +2,4 @@ user_pref('browser.newtabpage.activity-stream.feeds.topsites', true); // Shortcu
 user_pref("browser.search.suggest.enabled", true);
 user_pref('identity.fxaccounts.enabled', true);
 user_pref("gfx.canvas.accelerated", true); // enable if using a dedicated GPU on WINDOWS
+user_pref("signon.rememberSignons", true);
